8bb50195d1007b25c9d47caf7c853887a603cccd
maxjanney <70174153+maxjanney@users.noreply.github.com>
2021-11-05 12:16:11 -0400

    Update skip.js

--- skip.js
+++ skip.js
@@ -1,23 +1,23 @@
 const buttonClasses = [
-  "ytp-ad-skip-button ytp-button", // close ad button class
-  "ytp-ad-overlay-close-button", // Close overlay button class
+  "ytp-ad-skip-button ytp-button", // skippable ads
+  "ytp-ad-overlay-close-button", // overlay ads
 ];
 
 function simulateClick(button) {
   button.dispatchEvent(
     new MouseEvent("click", {
       view: window,
-      cancelable: true,
       bubbles: true,
+      cancelable: true,
     })
   );
 }
 
 setInterval(function () {
-  const buttons = buttonClasses.flatMap((b) => {
-    Array.from(document.getElementsByClassName(b)) || [];
-  });
-  for (const button of buttons) {
+  const buttons = buttonClasses.flatMap(
+    (b) => Array.from(document.getElementsByClassName(b)) || []
+  );
+  buttons.forEach((button) => {
     simulateClick(button);
-  }
-}, 1500);
+  });
+}, 2000);
